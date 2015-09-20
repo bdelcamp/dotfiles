7ec1452c4ad81c4a579b37f2f5ede2091f7080cc
hexadecimal-rainbow <mmkaz1998@gmail.com>
2015-09-20 14:06:48 -0500

    edited symlinks.sh

--- symlinks.sh
+++ symlinks.sh
@@ -3,7 +3,6 @@ ln -s ~/.dotfiles/Xresources ~/.Xresources
 ln -s ~/.dotfiles/vimrc ~/.vimrc
 ln -s ~/.dotfiles/vimperatorrc ~/.vimperatorrc
 ln -s ~/.dotfiles/vimperator ~/.vimperator
-ln -s ~/.dotfiles/startpage1 ~/.startpage
 ln -s ~/.dotfiles/i3 ~/.i3
 ln -s ~/.dotfiles/config/mimeapps.list ~/.config/mimeapps.list
 ln -s ~/.dotfiles/config/compton.conf ~/.config/compton.conf
